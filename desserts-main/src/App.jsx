4c7072d3c5fe017ba30f7f07755758762a329364
vakogabiani <147316277+vakogabiani@users.noreply.github.com>
2024-12-08 12:17:43 +0400

    Add files via upload

---
+++ desserts-main/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import { AddToCart, RemoveItem } from "./Icons"
+import dessertsProducts from "./data.json"
+
+// const BASE_URL = "https://res.cloudinary.com/dc2c49xov/desserts"
+
+function App() {
+  return (
+    <>
+      <h1>Hello World ❄</h1>
+      <RemoveItem />
+    </>
+  )
+}
+
+export default App
